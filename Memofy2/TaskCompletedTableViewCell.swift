3476624b5c385ba1cb1d640f7003e43253d4001c
jacky <jackychenada@icloud.com>
2021-04-03 13:51:04 +0700

    checkpoint

---
+++ Memofy2/TaskCompletedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskCompletedTableViewCell.swift
+//  Memofy2
+//
+//  Created by JACKY on 03/04/21.
+//
+
+import UIKit
+
+class TaskCompletedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
