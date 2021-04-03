1e44f95e43d37ee5ccd4dac6a554cf415866f480
jacky <jackychenada@icloud.com>
2021-04-04 00:39:27 +0700

    study plan main

--- Memofy2/ViewController.swift
+++ Memofy2/ViewController.swift
@@ -40,12 +40,20 @@ class Task: NSObject, NSCoding
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
-    var tasks: [Task] = []
-    var memos: [Task] = []
+    
+    let sections = ["To Do", "Completed"]
     let defaults = UserDefaults.standard
+    
+    var tasks: [Task] = [] //FEED
+    var memos: [Task] = [] //TEMP ARCHIVER
+    
+    var todoPlans: [Task] = []
+    var completedPlans: [Task] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.9490196078, blue: 0.9725490196, alpha: 1)
         // sometest test
         func addTask(name: String, status: String){
             let newTask = Task(name: name, status: status)
@@ -65,6 +73,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         print("INI MEMOS", memos as Any)
         addMemo(name: "programmer", status: "in progress")
         
+        for memo in memos {
+            if memo.status == "in progress" {
+                todoPlans.append(memo)
+            }
+            else if memo.status == "completed" {
+                completedPlans.append(memo)
+            }
+        }
+        print("TODO PLANS:", todoPlans)
+        print("completed PLANS:", completedPlans)
 //        addTask(name: "makan", status: "in progress")
 //        addTask(name: "minum", status: "in progress")
 //        addTask(name: "tidur", status: "completed")
@@ -84,19 +102,69 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.register(nib, forCellReuseIdentifier: "MemoTableViewCell")
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 30
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        //self.tableView.estimatedRowHeight = 120
+        return UITableView.automaticDimension
+    }
+//
+//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30.0
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return memos.count
+        //return memos.count
+        if section == 0 {
+            return todoPlans.count
+        }
+        else {
+            return completedPlans.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 30))
+        headerView.backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.9490196078, blue: 0.9725490196, alpha: 1)
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 30))
+        label.text = sections[section]
+        headerView.addSubview(label)
+        return headerView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MemoTableViewCell", for: indexPath) as! MemoTableViewCell
-                cell.nameLabel.text = memos[indexPath.row].name + " memo"
-                cell.nameLabel.textColor = .red
-                cell.dateLabel.text = memos[indexPath.row].status
+                var tempData:Task
+                if(indexPath.section == 0){
+                    tempData = todoPlans[indexPath.row]
+                }else{
+                    tempData = completedPlans[indexPath.row]
+                }
+                let data = tempData
+                cell.nameLabel.text = data.name + " memo"
+                cell.nameLabel.textColor = #colorLiteral(red: 0, green: 0.3607843137, blue: 0.7254901961, alpha: 1)
+                cell.dateLabel.text = data.status
                 return cell
     }
+    
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        self.tableView.rowHeight = UITableView.automaticDimension
+//        self.tableView.estimatedRowHeight = UITableView.automaticDimension
+//    }
     
     
 }
