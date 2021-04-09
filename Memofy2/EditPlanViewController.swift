383b29090c2eb870befa28add94465ee63870e7f
AdindaPujiR <79858029+AdindaPujiR@users.noreply.github.com>
2021-04-10 02:59:01 +0700

    YabeSalahTempat
    - Switch Button masih belum bener
    - Button belum berfungsi

--- Memofy2/EditPlanViewController.swift
+++ Memofy2/EditPlanViewController.swift
@@ -7,18 +7,21 @@
 
 import UIKit
 
-class EditPlanViewController: UITableViewController {
-    
+class EditPlanViewController: UITableViewController, RepeatEditDataDelegate {
     
     var dateFormatterr = DateFormatter()
     
-    let plan = "Programming"
-    let note = "Swift Playground"
-    let status = "Completed"
+    var plans : [Plan] = []
+    var days : [Int] = []
+    
+    let planDummy = "Programming"
+    let noteDummy = "Swift Playground"
+    let statusDummy = "Completed"
     
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     
     @IBOutlet weak var repeatTableCell: UIView!
+    @IBOutlet weak var repeatEditLabel: UILabel!
     
     @IBOutlet weak var dateStartsTableCell: UIView!
     @IBOutlet weak var dateStartsLabel: UILabel!
@@ -54,6 +57,9 @@ class EditPlanViewController: UITableViewController {
         //Set default date di add
         dateStartsDatePicker.date = NSDate() as Date
         dateEndsDatePicker.date = NSDate() as Date
+        timeReminderTimePicker.date = NSDate() as Date
+        
+        hiddenViewDatePicker(fieldName: "init")
         
         dateFormatterr.dateStyle = DateFormatter.Style.long
         
@@ -61,17 +67,18 @@ class EditPlanViewController: UITableViewController {
         
         dateEndsLabel.text = dateFormatterr.string(from: dateEndsDatePicker.date)
         
-        if (status == "Completed") {
+        dateFormatterr.dateFormat = "HH:mm"
+        timeReminderTimeLabel.text = dateFormatterr.string(from: timeReminderTimePicker.date)
+        
+        if (statusDummy == "Completed") {
             startStudyButton.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
             startStudyButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
-        } else if (status == "To Do") {
+        } else if (statusDummy == "To Do") {
             startStudyButton.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
             startStudyButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         }
     }
     
-    @IBAction func saveStudyPlanButton(_ sender: Any) {
-    }
     @IBAction func deletePlanButton(_ sender: Any) {
     }
     
@@ -143,16 +150,48 @@ class EditPlanViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showRepeat" {
-//            let destination = segue.destination as! repeatDetails
-//            destination.delegate = self
-//            destination.sendRepeatDetails = "Permisi Kucing"
-            
+            let destination = segue.destination as! RepeatEditTableViewController
+            destination.delegate = self
+            destination.sendRepeatEditData = days
+
         }
     }
     
-    func receiveRepeatDetails(info: String) {
-        print(info)
-    }
+    func receivedRepeatEditData(day: [Int]) {
+        days = day
+        
+        if !day.isEmpty {
+            print("Jumlah", day.count)
+            if day.count == 1 {
+                var labelRepeat = ""
+                switch day[0] {
+                case 0:
+                    labelRepeat = "Every Monday"
+                case 1:
+                    labelRepeat = "Every Tuesday"
+                case 2:
+                    labelRepeat = "Every Wednesday"
+                case 3:
+                    labelRepeat = "Every Thursday"
+                case 4:
+                    labelRepeat = "Every Friday"
+                case 5:
+                    labelRepeat = "Every Saturday"
+                case 6:
+                    labelRepeat = "Every Sunday"
+                default:
+                    break
+                }
+                repeatEditLabel.text = labelRepeat
+            } else {
+                repeatEditLabel.text = "Multiple"
+            }
+            } else {
+                print("Kosong", day)
+                self.repeatEditLabel.text = "Never"
+            }
+        }
+
     
   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     if indexPath.section == 1 && indexPath.row == 2 {
@@ -164,8 +203,8 @@ class EditPlanViewController: UITableViewController {
         return height
     }
     else if indexPath.section == 1 && indexPath.row == 6 {
-        let height:CGFloat = timeReminderTimePicker.isHidden ? 0.0 : 58.0
-        return height
+//        let height:CGFloat = timeReminderTimePicker.isHidden ? 0.0 : 58.0
+        return timeReminderSwitch.isOn ? 55.0 : 0.0
     }
     else if indexPath.section == 2 && indexPath.row == 1 {
         let height:CGFloat = studyDurationTimePicker.isHidden ? 0.0 : 280.0
@@ -181,7 +220,7 @@ class EditPlanViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let startDateIndexPath = NSIndexPath (row:1, section: 1)
         let endDateIndexPath = NSIndexPath (row: 3, section: 1)
-        let timeReminderIndexPath = NSIndexPath (row:5, section: 1)
+//        let timeReminderIndexPath = NSIndexPath (row:5, section: 1)
         let studyDurationIndexPath = NSIndexPath (row: 0, section: 2)
         let breakDurationIndexPath = NSIndexPath (row: 2, section: 2)
         
@@ -194,15 +233,15 @@ class EditPlanViewController: UITableViewController {
                 hiddenViewDatePicker(fieldName: "dateEnds")
                 animationPicker(iP: indexPath)
             }
-            else if timeReminderIndexPath as IndexPath == indexPath {
-                dateStartsDatePicker.isHidden = true
-                dateEndsDatePicker.isHidden = true
-                timeReminderTimePicker.isHidden = !timeReminderTimePicker.isHidden
-                studyDurationTimePicker.isHidden = true
-                breakDurationTimePicker.isHidden = true
-                
-                animationPicker(iP: indexPath)
-            }
+//            else if timeReminderIndexPath as IndexPath == indexPath {
+//                dateStartsDatePicker.isHidden = true
+//                dateEndsDatePicker.isHidden = true
+//                timeReminderTimePicker.isHidden = !timeReminderTimePicker.isHidden
+//                studyDurationTimePicker.isHidden = true
+//                breakDurationTimePicker.isHidden = true
+//
+//                animationPicker(iP: indexPath)
+//            }
             else if studyDurationIndexPath as IndexPath == indexPath {
                 hiddenViewDatePicker(fieldName: "studyDuration")
                 animationPicker(iP: indexPath)
---
+++ Memofy2/RepeatEditTableViewController.swift
@@ -0,0 +1,147 @@
+//
+//  RepeatEditTableViewController.swift
+//  Memofy2
+//
+//  Created by Adinda Puji Rahmawaty on 09/04/21.
+//
+
+import UIKit
+
+protocol RepeatEditDataDelegate: AnyObject {
+    func receivedRepeatEditData(day : [Int])
+}
+
+class RepeatEditTableViewController: UITableViewController {
+    
+//    var repeatDataEdit: String!
+    
+    weak var delegate: RepeatEditDataDelegate? = nil
+    
+    var sendRepeatEditData : [Int] = []
+    
+    var days = Set<Int>()
+
+    
+    @IBOutlet var repeatEditTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        days = Set(sendRepeatEditData)
+        print("Ini adalah Array", sendRepeatEditData)
+        
+    }
+    
+    // lol, bukan ketik sendiri, tapi pake "viewWillDissapear
+//    override func backToEdit(_ animated:Bool) {
+//    super.backToEdit (animated)
+//
+//        if self.isMovingFromParent {
+//            let myArray = Array(days).sorted()
+//            delegate?.receivedRepeatEditData(day: myArray)
+//        }
+//    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if self.isMovingFromParent {
+            let myArray = Array(days).sorted()
+            delegate?.receivedRepeatEditData(day: myArray)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if repeatEditTableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            self.repeatEditTableView.cellForRow(at: indexPath)?.accessoryType = .none
+        days.remove(indexPath.row)
+        }
+        else {
+            self.repeatEditTableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            days.insert(indexPath.row)
+        }
+        self.repeatEditTableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if let identifier =  cell.reuseIdentifier {
+            switch identifier {
+            case "Monday": cell.accessoryType = days.contains(0) ? .checkmark : .none
+            case "Tuesday": cell.accessoryType = days.contains(1) ? .checkmark : .none
+            case "Wednesday": cell.accessoryType = days.contains(2) ? .checkmark : .none
+            case "Thursday": cell.accessoryType = days.contains(3) ? .checkmark : .none
+            case "Friday": cell.accessoryType = days.contains(4) ? .checkmark : .none
+            case "Saturday": cell.accessoryType = days.contains(5) ? .checkmark : .none
+            case "Sunday": cell.accessoryType = days.contains(6) ? .checkmark : .none
+            default: break
+            }
+        }
+    }
+}
+    
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
